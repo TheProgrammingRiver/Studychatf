a0394fd9f0438dd674a68e65637d223d9ba51360
River <riverdejesus2@gmail.com>
2023-10-26 10:34:16 -0400

    Create app-routing.module.ts

---
+++ Studychatf/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { StudyRoomListComponent } from './study-room-list/study-room-list.component';
+import { ChatRoomComponent } from './chat-room/chat-room.component';
+
+
+const routes: Routes = [
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'study-room-list', component: StudyRoomListComponent },
+  { path: 'chat-room', component: ChatRoomComponent },
+  { path: 'rooms', component: StudyRoomListComponent },
+  { path: '**', redirectTo: '/login' },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
