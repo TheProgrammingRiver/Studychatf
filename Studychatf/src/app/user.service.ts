e1f002d78269e342d0fc24a4b393601a1f64ac59
River <riverdejesus2@gmail.com>
2023-10-26 10:35:38 -0400

    Create user.service.ts

---
+++ Studychatf/src/app/user.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  [x: string]: any;
+  
+
+  private baseUrl = 'http://localhost:8080/users';
+
+  private currentUser: any = null;
+
+constructor(private http: HttpClient) { }
+
+  setCurrentUser(user: any) {
+    this['currentUserData'] = user;
+    this.currentUser = user;
+  }
+
+  getCurrentUser() {
+    return this.currentUser;
+  }
+
+  onLoginSuccess(response: any) {
+    this.setCurrentUser(response.user);
+    this.currentUser = response.user;
+  }
+  
+
+
+  register(user: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/register`, user);
+  }
+
+  login(credentials: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/login`, credentials);
+  }
+
+  getUserDetails(userId: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${userId}`);
+  }
+
+  getUserByUsername(username: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/username/${username}`);
+  }
+
+  listRoomsForUser(userId: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${userId}/rooms`);
+  }
+}
